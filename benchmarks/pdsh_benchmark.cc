45afedbc8b59e8b2f3bcd9e552f3017d76ea17ca
Wonsup Yoon <pusnow@kaist.ac.kr>
2024-10-25 00:04:02 +0900

    [PDS-H]  Init Makefile

---
+++ benchmarks/pdsh_benchmark.cc
@@ -0,0 +1,13 @@
+
+int main(int, char *[]) {
+
+    return (0);
+}
+
+// -----------------------------------------------------------------------------
+
+// Local Variables:
+// mode:C++
+// tab-width:4
+// c-basic-offset:4
+// End:
